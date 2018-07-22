8a8584835a22143af04829c8d56dfa4ebdb3b498
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-07-21 18:30:06 -0600

    space after comma

--- src/Actions/Actions.test.js
+++ src/Actions/Actions.test.js
@@ -2,7 +2,7 @@ import * as actions from './Actions';
 
 describe('actions', ()=>{
 
-  it('should return a type of INPUT_LOCATION and location key',() => {
+  it('should return a type of INPUT_LOCATION and location key', () => {
     const location = 'Denver';
     const expected = {
       type: 'INPUT_LOCATION',
