e812367de5120ad18e21e9bbde4eb4aa82d5fd7d
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-07-30 17:56:23 -0600

    Add mstp test to lpgstationlist

--- src/Containers/LpgStationList/LpgStationList.test.js
+++ src/Containers/LpgStationList/LpgStationList.test.js
@@ -26,5 +26,40 @@ describe('LpgStationList Container', () => {
       expect(wrapper).toMatchSnapshot();
     });
   });
+
+
+  describe('mapStateToProps', () => {
+    let wrapper;
+    let mockProps;
+    const mockHandleFilterClick = jest.fn();
+    beforeEach(() => {
+      mockProps = {
+        stations: [{ name: 'bubbas' }],
+        location: '61615',
+        filter: 'SHOW_ELEC'
+      };
+      wrapper = shallow(
+        <LpgStationList
+          {...mockProps}
+          handleFilterClick={mockHandleFilterClick}
+        />);
+    });
+
+    it('should return an object with keys location, stations, and filter', () => {
+      const mockState = {
+        location: '61615',
+        stations: [{ name: 'bubbas' }],
+        filter: 'SHOW_LPG'
+      };
+      const expected = {
+        location: '61615',
+        stations: [{ name: 'bubbas' }],
+        filter: 'SHOW_LPG'
+      };
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+    });
+  }); 
   
 });
