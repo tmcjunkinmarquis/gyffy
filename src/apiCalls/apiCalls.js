247eda78227449a12827526be1fbed5e8ed64a5d
Theresa Marquis <tmcjunkinmarquis@gmail.com>
2022-01-16 14:46:26 -0600

    Add use yarn update apiKey

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -2,6 +2,7 @@ import { apiKey } from '../apiKey';
 
 export const locationFetch = async (location)=>{
   const zipcode = location;
+  // const url = https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=E85,ELEC&state=CA&limit=2&api_key=${apiKey}
   const url = `https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=${apiKey}&location=${zipcode}`;
   const response = await fetch(url);
   const stationData = await response.json();
